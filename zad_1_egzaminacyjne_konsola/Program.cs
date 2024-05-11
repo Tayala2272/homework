2e337a5f17232a59bbb820f50a1aa6d9dabebdfd
Tayala2272 <szyszczka@gmail.com>
2024-05-11 11:57:58 +0200

    first two

---
+++ zad_1_egzaminacyjne_konsola/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+/*
+*********************************************************************
+*
+    klasa:
+        Osoba
+
+    opis:
+        Klasa Osoba reprezentuje osobę uczestniczącą w systemie forum użytkowników.
+        Zawiera prywatne pola dla identyfikatora i imienia osoby oraz statyczne pole
+        zliczające liczbę instancji klasy.
+
+    pola:
+        int id - prywatny identyfikator osoby
+        string imie - prywatna nazwa osoby
+        static int LiczbaInstancji - ogólnodostępne pole statyczne zliczające instancje klasy
+
+    autor:
+        Bartek Pyka
+
+*********************************************************************
+*/
+class Osoba
+{
+    private int id;
+    private string imie;
+    public static int LiczbaInstancji = 0;
+
+    public Osoba()
+    {
+        this.id = 0;
+        this.imie = string.Empty;
+        LiczbaInstancji++;
+    }
+
+    public Osoba(int id, string imie)
+    {
+        this.id = id;
+        this.imie = imie;
+        LiczbaInstancji++;
+    }
+
+    public Osoba(Osoba innaOsoba)
+    {
+        this.id = innaOsoba.id;
+        this.imie = innaOsoba.imie;
+        LiczbaInstancji++;
+    }
+
+    public void WypiszImie(string argument)
+    {
+        if (!string.IsNullOrEmpty(this.imie))
+        {
+            Console.WriteLine($"Cześć {argument}, mam na imię {this.imie}");
+        }
+        else
+        {
+            Console.WriteLine("Brak danych");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Osoba osoba1 = new Osoba();
+        Osoba osoba2 = new Osoba(1, "Janusz");
+        Osoba osoba3 = new Osoba(osoba2);
+
+        osoba1.WypiszImie("Marcin");
+        osoba2.WypiszImie("Marcin");
+        osoba3.WypiszImie("Michał");
+
+        Console.WriteLine($"Liczba stworzonych instancji klasy Osoba: {Osoba.LiczbaInstancji}");
+    }
+}
---
+++ zad_2_egzaminacyjne_konsola/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/*
+*********************************************************************
+*
+    klasa:
+        Program
+
+    opis:
+        Aplikacja konsolowa do obliczania Największego Wspólnego Dzielnika (NWD)
+        dwóch liczb całkowitych dodatnich za pomocą algorytmu Euklidesa.
+
+    autor:
+        Bartek Pyka
+
+*********************************************************************
+*/
+
+
+
+class Program
+{
+    // Funkcja do znajdowania NWD dwóch liczb
+    static int NWD(int a, int b)
+    {
+        while (a!=b)
+        {
+            if(a>b)
+            {
+                a = a - b;
+            }
+            else
+            {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Podaj dwie liczby całkowite dodatnie:");
+
+        Console.Write("a = ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a > 0 && b > 0)
+        {
+            int wynik = NWD(a, b);
+            Console.WriteLine($"NWD = {wynik}");
+        }
+        else
+        {
+            Console.WriteLine("Podaj liczby całkowite dodatnie.");
+        }
+    }
+}
